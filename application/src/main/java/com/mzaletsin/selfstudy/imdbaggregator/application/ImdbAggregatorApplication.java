7244dc8a65652e456d3fc4ea955a100c67a236be
Maksym Zalietin <lappid.mail@gmail.com>
2023-05-12 11:57:01 +0200

    Remove maven-publish plugin

--- application/src/main/java/com/mzaletsin/selfstudy/imdbaggregator/application/ImdbAggregatorApplication.java
+++ application/src/main/java/com/mzaletsin/selfstudy/imdbaggregator/application/ImdbAggregatorApplication.java
@@ -2,8 +2,6 @@ package com.mzaletsin.selfstudy.imdbaggregator.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @SpringBootApplication
 public class ImdbAggregatorApplication {
--- domain/src/main/java/module-info.java
+++ domain/src/main/java/module-info.java
@@ -1,4 +1,3 @@
-@SuppressWarnings("requires-transitive-automatic")
 module imdb.aggregator.domain {
     requires transitive jakarta.validation;
     exports com.mzaletsin.selfstudy.imdbaggregator.domain.entity;
