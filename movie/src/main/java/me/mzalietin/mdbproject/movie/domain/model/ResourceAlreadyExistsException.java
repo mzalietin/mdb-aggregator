079b59267a74ab24453d21d10f014d19a5eaef95
Maksym Zalietin <maksym.zalietin@gmail.com>
2026-03-24 14:04:40 +0100

    Config update

--- movie/src/main/java/me/mzalietin/mdbproject/movie/domain/model/ResourceAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package me.mzalietin.mdbproject.movie.domain.model;
-
-public class ResourceAlreadyExistsException extends RuntimeException {
-    public ResourceAlreadyExistsException(String message) {
-        super(message);
-    }
-}
